bc68b97444317e59c1f6ce87a11a579e9f495125
Abdul Ahad <53481272+abahad7921@users.noreply.github.com>
2021-05-12 13:26:01 +0530

    style: removed listStyle

--- src/App.js
+++ src/App.js
@@ -2,23 +2,33 @@ import "./styles.css";
 import React, { useState } from "react";
 
 export default function App() {
-  function inputChangeHandler(event) {
-    console.log(event.target.value);
-    setTask(event.target.value);
-  }
-
   const [todos, setTodos] = useState(["Dummy Task"]);
   const [input, setInput] = useState("");
+
+  // const addTodo = (event) => {
+  //   //this will fire off when we click add to do
+  // }
+
+  function addTodo(event) {
+    console.log("add todo working");
+    setTodos([...todos, input]);
+  }
+
   return (
     <div className="App">
-      <h1>React Todo App</h1>
+      <h1>React Todo App ✅</h1>
       <h2>Start adding your work!</h2>
-
-      <input placeholder="write a task" onChange={inputChangeHandler} />
-      <button>Add Task</button>
+      <form>
+        <input
+          placeholder="write a task"
+          onChange={(event) => setInput(event.target.value)}
+          value={input}
+        />
+        <button onClick={addTodo}>Add Task</button>
+      </form>
       <ul>
         {todos.map(function (todo) {
-          return <li>{todo}</li>;
+          return <li style={{ listStyle: "none" }}>{todo}</li>;
         })}
       </ul>
     </div>
