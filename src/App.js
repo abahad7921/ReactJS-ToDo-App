d10ed7aab92cf292bc58f3577d165be47a1c7690
Abdul Ahad <53481272+abahad7921@users.noreply.github.com>
2021-05-12 13:06:15 +0530

    fix: removed syntax errors

--- src/App.js
+++ src/App.js
@@ -8,14 +8,13 @@ export default function App() {
   }
 
   const [todos, setTodos] = useState(["Dummy Task"]);
-
+  const [input, setInput] = useState("");
   return (
     <div className="App">
       <h1>React Todo App</h1>
       <h2>Start adding your work!</h2>
 
-      <input placeholder="write a task" onChange={inputChangeHandler}></input>
-
+      <input placeholder="write a task" onChange={inputChangeHandler} />
       <button>Add Task</button>
       <ul>
         {todos.map(function (todo) {
