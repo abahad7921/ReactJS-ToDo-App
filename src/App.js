94a102e53a1cd6f50d3cd14b3cd302f7c899cab3
Abdul Ahad <53481272+abahad7921@users.noreply.github.com>
2021-05-12 12:45:55 +0530

    Update: Delete Task added

--- src/App.js
+++ src/App.js
@@ -2,29 +2,23 @@ import "./styles.css";
 import React, { useState } from "react";
 
 export default function App() {
-  function inputChangeHandler(event){
+  function inputChangeHandler(event) {
     console.log(event.target.value);
     setTask(event.target.value);
   }
-  
-  const [task,setTask] = useState([]);
-  
+
+  const [task, setTask] = useState([]);
+
   return (
     <div className="App">
       <h1>React Todo App</h1>
       <h2>Start adding your work!</h2>
 
-      <input placeholder="write a task" onChange={inputChangeHandler}>
-      </input>
+      <input placeholder="write a task" onChange={inputChangeHandler}></input>
 
       <button>Add Task</button>
-
-      <ul>
-        {
-          <li>{task}</li>
-        }
-      </ul>
-
+      <button>Delete Task</button>
+      <ul>{<li>{task}</li>}</ul>
     </div>
   );
 }
